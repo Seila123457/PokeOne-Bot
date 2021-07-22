3f10a44f1e3c0ad249baa083bc2b6e7dee64e92d
Xablau <64701243+Seila123457@users.noreply.github.com>
2021-07-22 21:54:53 +0100

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/PegarPokemon.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class PegarPokemon
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Moverato = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.move4 = new System.Windows.Forms.CheckBox();
+            this.move3 = new System.Windows.Forms.CheckBox();
+            this.move2 = new System.Windows.Forms.CheckBox();
+            this.move1 = new System.Windows.Forms.CheckBox();
+            this.fugir = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Moverato
+            // 
+            this.Moverato.Location = new System.Drawing.Point(12, 177);
+            this.Moverato.Name = "Moverato";
+            this.Moverato.Size = new System.Drawing.Size(146, 35);
+            this.Moverato.TabIndex = 0;
+            this.Moverato.Text = "Caçar pokemon";
+            this.Moverato.UseVisualStyleBackColor = true;
+            this.Moverato.Click += new System.EventHandler(this.Moverato_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Só lateral";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // move4
+            // 
+            this.move4.AutoSize = true;
+            this.move4.Location = new System.Drawing.Point(12, 120);
+            this.move4.Name = "move4";
+            this.move4.Size = new System.Drawing.Size(72, 21);
+            this.move4.TabIndex = 2;
+            this.move4.Text = "Move4";
+            this.move4.UseVisualStyleBackColor = true;
+            // 
+            // move3
+            // 
+            this.move3.AutoSize = true;
+            this.move3.Location = new System.Drawing.Point(12, 93);
+            this.move3.Name = "move3";
+            this.move3.Size = new System.Drawing.Size(72, 21);
+            this.move3.TabIndex = 3;
+            this.move3.Text = "Move3";
+            this.move3.UseVisualStyleBackColor = true;
+            // 
+            // move2
+            // 
+            this.move2.AutoSize = true;
+            this.move2.Location = new System.Drawing.Point(12, 66);
+            this.move2.Name = "move2";
+            this.move2.Size = new System.Drawing.Size(72, 21);
+            this.move2.TabIndex = 4;
+            this.move2.Text = "Move2";
+            this.move2.UseVisualStyleBackColor = true;
+            // 
+            // move1
+            // 
+            this.move1.AutoSize = true;
+            this.move1.Location = new System.Drawing.Point(12, 39);
+            this.move1.Name = "move1";
+            this.move1.Size = new System.Drawing.Size(72, 21);
+            this.move1.TabIndex = 5;
+            this.move1.Text = "Move1";
+            this.move1.UseVisualStyleBackColor = true;
+            // 
+            // fugir
+            // 
+            this.fugir.AutoSize = true;
+            this.fugir.Location = new System.Drawing.Point(12, 150);
+            this.fugir.Name = "fugir";
+            this.fugir.Size = new System.Drawing.Size(115, 21);
+            this.fugir.TabIndex = 6;
+            this.fugir.Text = "Fugir Sempre";
+            this.fugir.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(199, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 200);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Battlefrontiend";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PegarPokemon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 227);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fugir);
+            this.Controls.Add(this.move1);
+            this.Controls.Add(this.move2);
+            this.Controls.Add(this.move3);
+            this.Controls.Add(this.move4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Moverato);
+            this.Name = "PegarPokemon";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Moverato;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox move4;
+        private System.Windows.Forms.CheckBox move3;
+        private System.Windows.Forms.CheckBox move2;
+        private System.Windows.Forms.CheckBox move1;
+        private System.Windows.Forms.CheckBox fugir;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/PegarPokemon.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoItX3Lib;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class PegarPokemon : Form
+    {
+        AutoItX3 au3 = new AutoItX3();
+        public object[] pixCoord;
+
+        public PegarPokemon()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void Moverato_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                  backgroundWorker1.CancelAsync();  
+            }
+        }
+        
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            Random randNum = new Random();
+            string[] andar = new string[] {"www", "sss", "ddd","aaa"};
+            while (true)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    break;
+                    
+                }
+                if (checkBox1.Checked)
+                {
+                    moverBoneco("ddd", 2);
+                    moverBoneco("aaa", 2);
+                }
+                else
+                {
+                    moverBoneco(andar[randNum.Next(4)], 2);
+                }
+                if(detectarPixel(0xB22D29, 0, 0, 1920, 1080) == true)
+                {
+                    if (detectarPixel(0x512F00, 0, 0, 1920, 1080) == true) 
+                    {
+                        MessageBox.Show("ALGO ESPECIAL ENCONTRADO MAS NAO IDENTIFICADO :(");
+                        break;
+                    }
+                    else
+                    {
+                        if (!fugir.Checked)
+                            atacar();
+                        else
+                            au3.MouseClick("LEFT", 934, 830, 1, 2);
+                    }
+                }
+            }
+        }
+        bool detectarPixel(int pixel,int xcima,int ycima ,int xbaixo, int ybaixo)
+        {
+            Object pix = au3.PixelSearch(xcima, ycima, 1920, 1080,pixel);
+            if (pix.ToString() != "1")
+            {
+                try
+                {
+                    pixCoord = (object[])pix;
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        void atacar()
+        {
+            au3.MouseClick("LEFT", 772, 780, 1, 2);
+            Thread.Sleep(500);
+            if (detectarPixel(0x828282, 917, 882,1920,1080) == false && move1.Checked)
+            {
+                Thread.Sleep(50);
+                au3.MouseClick("LEFT", 730, 709, 1, 2);//Move 1
+            }
+            else if (detectarPixel(0x828282, 1145, 879,1920,1080) == false && move2.Checked)
+            {
+                Thread.Sleep(50);
+                au3.MouseClick("LEFT", 924, 709, 1, 2);//Move 2
+            }
+            else if (detectarPixel(0x828282, 908, 960,1920,1080) == false && move3.Checked)
+            {
+                Thread.Sleep(50);
+                au3.MouseClick("LEFT", 685, 771, 1, 2);//Move 3
+            }
+            else if (move4.Checked)
+            {
+                Thread.Sleep(50);
+                au3.MouseClick("LEFT", 911, 779, 1, 2);//Move 4
+            }
+        }
+        void moverBoneco (string direcao,int mover)
+        {
+            for(int i = 0; i < mover;i++)
+            {
+                au3.Send(direcao);
+                Thread.Sleep(300);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("is to come!!");
+        }
+    }
+}
